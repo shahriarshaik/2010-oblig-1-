519df190c6d3df1291c1eb2b4ae1cda3035fecb0
Shahriar <meherss@uio.no>
2022-09-06 12:11:06 +0200

    bytta arraylist med Hashset

--- Set.java
+++ Set.java
@@ -1,10 +1,15 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-public class tree {
+
+import javax.print.DocFlavor.INPUT_STREAM;
+
+import java.util.HashSet;
+public class Set {
     static ArrayList<Integer> liste = new ArrayList<>();
-    static void contains(){System.out.println(liste);}
-    static void insert(int i){liste.add(i);}
-    static void remove(int i){try {liste.remove(i);} catch (Exception e) {System.out.println("finnes ikke");}}
+    static HashSet<Integer> tallliste = new HashSet<>();
+    static boolean contains(HashSet<Integer> set, int o){if(set.contains(o)){return true;} else {return false;}}
+    static boolean insert(HashSet<Integer> set, int o){try {set.add(o); return true;} catch (Exception e) {return false;}}
+    static boolean remove(HashSet<Integer> set, int o){try {set.remove(o); return true;} catch (Exception e) {return false;}}
     static void size(){System.out.println("size: " + liste.size());}
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -16,9 +21,9 @@ public class tree {
             String inputString = input.nextLine();
             String[] splitte = inputString.split(" ");
             try {
-                if(splitte[0].equals("contains")){contains();}
-                else if(splitte[0].equals("insert")){insert(Integer.parseInt(splitte[1]));}
-                else if(splitte[0].equals("remove")){remove(Integer.parseInt(splitte[1]));}
+                if(splitte[0].equals("contains")){contains(tallliste, Integer.parseInt(splitte[1]));}
+                else if(splitte[0].equals("insert")){insert(tallliste, Integer.parseInt(splitte[1]));}
+                else if(splitte[0].equals("remove")){remove(tallliste, Integer.parseInt(splitte[1]));}
                 else if(splitte[0].equals("size")){size();}
                 else{System.out.println("wrong input given ");i--;}
             } catch (Exception e) {}}}}
---
+++ prog.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class prog {
+    public static void main(String[] args) {
+        Set set = new Set();
+
+    }
+}
